1b463c904163c06ec8030329b48c9d2bfe6c1930
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2026-04-09 21:42:48 +0530

    create dto request for slot

---
+++ AppoinmentBookingSystem/appointmentBooking_/src/main/java/com/appointmentBooking/appointmentBooking/DTO/SlotRequest.java
@@ -0,0 +1,25 @@
+package com.appointmentBooking.appointmentBooking.DTO;
+
+import java.time.LocalDateTime;
+
+public class SlotRequest {
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+}
